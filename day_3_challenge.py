51535be90bcf6a5a883953cd7fd0506c71baead3
STayengco <137461370+STayengco@users.noreply.github.com>
2024-01-12 16:03:48 -0800

    completed day 3 challenge

--- day_3_challenge.py
+++ day_3_challenge.py
@@ -1,11 +1,15 @@
 # Write a function to count the number of vowels in a given string
+def count_vowels(string_to_count):
+    vowel_count = 0
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    for letter in string_to_count.lower():
+        if letter in vowels:
+            vowel_count +=1
+    
+    return vowel_count
 
 string_to_count = input("String to count vowels from: ")
-vowel_count = 0
-vowels = ['a', 'e', 'i', 'o', 'u']
+num_vowels = count_vowels(string_to_count)
 
-for letter in string_to_count.lower():
-    if letter in vowels:
-        vowel_count +=1
-
-print (f"There are {vowel_count} vowels in {string_to_count}.")
+print (f"There are {num_vowels} vowels in {string_to_count}.")
