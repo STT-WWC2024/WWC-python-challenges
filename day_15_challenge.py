31b12558846533a993cd583242d7760c4c99a8d4
STayengco <137461370+STayengco@users.noreply.github.com>
2024-01-24 23:46:59 -0800

    completed day 15 challenge

--- day_15_challenge.py
+++ day_15_challenge.py
@@ -7,5 +7,9 @@ print(f"{year} was a leap year: {calendar.isleap(year)}")
 # OR
 
 year = int(input("Enter a year\n"))
-leap = True if year %4 == 0 else False
+if year % 100 == 0 and year % 400 != 0:
+    leap = False
+else:
+    leap = True if year %4 == 0 else False
+    
 print(f"{year} was a leap year: {leap}")
