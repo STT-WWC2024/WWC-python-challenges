e0d646f784eeb3e55395e66d973ca21f7a2bb098
STayengco <137461370+STayengco@users.noreply.github.com>
2024-01-25 15:40:46 -0800

    completed day 16 challenge

---
+++ day_16_challenge.py
@@ -0,0 +1,18 @@
+# Write a function that counts the frequency of each word in a sentence.
+
+def count_unique(str):
+    unique_words = []
+    string_words = str.split()
+    for item in string_words:
+        if unique_words.count(item) == 0:
+            unique_words.append(item)
+    for word in unique_words:
+        if str.count(word) == 1:
+            print(f'"{word}" appears {str.count(word)} time')
+        else: 
+            print(f'"{word}" appears {str.count(word)} times')
+        
+original_string = input("Enter a string to count each word in: ")
+str = original_string.lower()
+print(f'In the original string "{original_string}", \n')
+count_unique(str)
