f155fad5f1f83af0faae3bf267de92c7d7646afc
STayengco <137461370+STayengco@users.noreply.github.com>
2024-02-15 01:51:58 -0800

    completed day 36 challenge

--- day_36_challenge.py
+++ day_36_challenge.py
@@ -1,21 +1,24 @@
 # Write a Python program to check if two strings are anagrams.
+import re
 
-string1 = "The hurricanes"
-string2 = "These churn air"
+def check_anagrams(string1, string2):
 
-str1 = string1.replace(" ", "").lower()
-str2 = string2.replace(" ", "").lower()
+    str1 = re.sub('[\W_]+', '', string1.lower())
+    str2 = re.sub('[\W_]+', '', string2.lower())
 
-anagrams = False
-
-if len(str1) == len(str2):
-    anagrams = True
-    for i in range(0, len(str1) - 1):
-        if str1.count(str1[i].lower()) != str2.count(str1[i].lower()):
-            anagrams = False
-            break
-
-else:
     anagrams = False
 
-print(f"{string1} and {string2} are anagrams: {anagrams}")
+    if len(str1) == len(str2):
+        anagrams = True
+        for i in range(0, len(str1) - 1):
+            if str1.count(str1[i]) != str2.count(str1[i]):
+                anagrams = False
+                break
+    else:
+        anagrams = False
+
+    print(f'"{string1}" and "{string2}" are anagrams: {anagrams}')
+
+check_anagrams("Creative", "Reactive")
+check_anagrams("The hurricanes", "These churn air")
+check_anagrams("To be or not to be: that is the question, whether 'tis nobler in the mind to suffer the slings and arrows of outrageous fortune", "In one of the Bard's best-thought-of tragedies, our insistent hero, Hamlet, queries on two fronts about how life turns rotten.")
