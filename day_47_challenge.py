761a015396c6b810f2ebf1a711def6a758713d40
STayengco <137461370+STayengco@users.noreply.github.com>
2024-02-27 17:47:41 -0800

    completed day 47 challenge

--- day_47_challenge.py
+++ day_47_challenge.py
@@ -3,14 +3,52 @@
 import math
 
 x = 5.6
-ans = math.ceil(x)
-print(f"The ceil of {x} is: {ans}")
+ans_ceil = math.ceil(x)
+ans_floor = math.floor(x)
+print(f"The ceil of {x} is: {ans_ceil}")
+print(f"The floor of {x} is: {ans_floor}")
 
+x = 8
+ans_factorial = math.factorial(x)
+print(f"The factorial of {x} is {ans_factorial}")
+
+x = 9
+y = 30
+ans_gcd = math.gcd(x, y)
+print(f"The gcd of {x} and {y} is: {ans_gcd}")
+
+x = -24
+ans_fabs = math.fabs(x)
+print(f"The absolute value of {x} is: {ans_fabs}")
+
+x = 8
+print (f"Euler's number, e, raised to the power of x is: {math.exp(x)}")
+
+x = 2
+y = 3
+ans_pow = math.pow(x, y)
+print(f"{x} to the power of {y} is: {ans_pow}")
+
+x = 2
+b = 3
+ans_log = math.log(x, b)
+print(f"Value of log {x} with base {b} is: {ans_log}")
+
+x = 8
+ans_log = math.log2(x)
+print(f"Value of log2 of {x} is: {ans_log}")
+
+x = 10000
+ans_log = math.log10(10000)
+print(f"The value of log10 of {x} is: {ans_log}")
+
+x = 25
+ans_log = math.sqrt(x)
+print(f"The square root of {x} is: {ans_log}")
+
+# et cetera
+# --- additional functions ---
 # math.copysign(x,y)
-# math.fabs(x)
-# math.factorial(x)
-# math.floor(x)
-# math.gcd(x,y)
 # math.fmod(x,y)
 # math.frexp(x)
 # math.fsum(iterable)
@@ -20,14 +58,8 @@ print(f"The ceil of {x} is: {ans}")
 # math.ldexp(x, i)
 # math.modf(x)
 # math.trunc(x)
-# math.exp(x)
 # math.expm1(x)
-# math.log(x[,b])
 # math.log1p(x)
-# math.log2(x)
-# math.log10(x)
-# math.pow(x,y)
-# math.sqrt(x)
 # math.acos(x)
 # math.asin(x)
 # math.atan(x)
