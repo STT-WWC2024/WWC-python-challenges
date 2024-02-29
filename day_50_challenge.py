2a1ff1f673ca7bc474f825d5df7a7db9134f0a25
STayengco <137461370+STayengco@users.noreply.github.com>
2024-02-28 21:55:39 -0800

    completed day 50 challenge

---
+++ day_50_challenge.py
@@ -0,0 +1,7 @@
+# Create a program that finds the intersection and union of two sets.
+
+set1 = set(['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+set2 = set(['e', 'f', 'g', 'h', 'i', 'j', 'k'])
+
+print(f"Union of set1 and set2: {set1.union(set2)}")
+print(f"Intersection of set1 and set2: {set1.intersection(set2)}")
